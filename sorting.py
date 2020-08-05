ac9a777cc836d97ae37f7c964fda2ca9337c4576
vohungson <vohungson@gmail.com>
2020-08-05 08:56:59 +0700

    update code for 2nd time

--- sorting.py
+++ sorting.py
@@ -1,3 +1,27 @@
 '''
 Python code for sorting
 '''
+
+class Sorting:
+    def __init__(self,array):
+        print("Python code for sorting")
+        print("Original Array: ", array)
+        print("Selection Sort: ", self.selectionSort(array))
+        print("Bubble Sort: ", self.bubbleSort(array))
+
+    def bubbleSort(self, array):
+        for i in range(len(array)-1):
+            for j in range(i+1, len(array)):
+                if array[i] > array[j]: array[i], array[j] = array[j], array[i]
+        return array
+
+    def selectionSort(self, array):
+        for i in range(len(array)-1):
+            minIndex = i
+            for j in range(i+1, len(array)):
+                if array[minIndex] > array[j]: minIndex = j
+            array[i], array[minIndex] = array[minIndex], array[i]
+        return array
+
+array = [64, 25, 12, 22, 11]
+Sorting(array)
